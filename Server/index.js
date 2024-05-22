eeeda30bf48705b48c8d33dceb8275f2e7cbc36a
Aryan Sarang <thearyansarang@gmail.com>
2024-05-22 14:24:44 +0530

    Connect mongodb server

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,22 @@
 import express from 'express';
+
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGOURL).then(() => {
+    console.log("Connected to MongoDB server");
+}).catch((err) => {
+    console.log(err);
+})
+
 const app = express();
 
+
+
+
+
+// run sever
 app.listen(3000, () => {
     console.log("Server running on port 3000")
 })
